a4abd2f906d4e1c25b373ce58f8cad47e116eebc
rottabonus <felix.hallenberg@myy.haaga-helia.fi>
2019-02-26 20:44:41 +0200

    create new customer

--- src/services/customers.js
+++ src/services/customers.js
@@ -4,4 +4,13 @@ const fetchAll = async () => {
   let data = await response.json()
   return data.content
 }
-export default { fetchAll }
+
+const create = async (object) => {
+  let response = await fetch('http://customerrest.herokuapp.com/api/customers',
+  { method: "POST",
+    mode: "cors",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(object) })
+  return response.json()
+}
+export default { fetchAll, create }
