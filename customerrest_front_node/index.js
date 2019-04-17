db69e89e0d758a78e2e8a1e9963cbfbb6445911e
rottabonus <felix.hallenberg@myy.haaga-helia.fi>
2019-04-17 08:21:58 +0300

    heroku port

--- customerrest_front_node/index.js
+++ customerrest_front_node/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.static('build'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
