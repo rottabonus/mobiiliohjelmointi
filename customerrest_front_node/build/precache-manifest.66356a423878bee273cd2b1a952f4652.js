3d1960dc9f5644dc25558468f5f028fbb117594d
rottabonus <felix.hallenberg@myy.haaga-helia.fi>
2019-04-17 08:09:31 +0300

    build

---
+++ customerrest_front_node/build/precache-manifest.66356a423878bee273cd2b1a952f4652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "088721ccbba5c2e50a71",
+    "url": "/static/css/main.2fd5ecdb.chunk.css"
+  },
+  {
+    "revision": "088721ccbba5c2e50a71",
+    "url": "/static/js/main.088721cc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f6fc1937714ec5d7a5f5",
+    "url": "/static/css/2.3e6c9871.chunk.css"
+  },
+  {
+    "revision": "f6fc1937714ec5d7a5f5",
+    "url": "/static/js/2.f6fc1937.chunk.js"
+  },
+  {
+    "revision": "bf3627bb148edf8aa22dc0f6b82192ba",
+    "url": "/index.html"
+  }
+];
